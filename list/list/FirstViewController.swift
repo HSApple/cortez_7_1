f7d897645d13f50ae792948eb942d2d1a001c6bc
Eduardo Cortez <farside13@Eduardos-MacBook-Pro.local>
2019-07-03 09:16:51 -0500

    adding todo

--- list/list/FirstViewController.swift
+++ list/list/FirstViewController.swift
@@ -8,7 +8,50 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var table: UITableView!
+    var items: [String] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style:UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+        }
+        
+        table.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            
+            items.remove(at: indexPath.row)
+            
+            table.reloadData()
+            
+            UserDefaults.standard.set(items, forKey: "items")
+            
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- list/list/SecondViewController.swift
+++ list/list/SecondViewController.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    @IBOutlet weak var newTask: UITextField!
+    @IBAction func addNewTask(_ sender: Any) {
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items:[String]
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+            items.append(newTask.text!)
+            
+            print(items)
+            
+        } else {
+            
+            items = [newTask.text!]
+            
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        newTask.text = ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
