28e231cf64a16ee2307d6159f53c093b9ee5ab7c
Eduardo Cortez <farside13@Eduardos-MacBook-Pro.local>
2019-07-03 09:01:40 -0500

    Adding web content

---
+++ list/list/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  list
+//
+//  Created by Eduardo Cortez on 7/2/19.
+//  Copyright © 2019 Farside 13. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ list/list/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  list
+//
+//  Created by Eduardo Cortez on 7/2/19.
+//  Copyright © 2019 Farside 13. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
